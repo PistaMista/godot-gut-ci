535db30935ba0fde2091b86b63104f62a21bf16d
Kryštof Albrecht <k@nymeria.tech>
2022-07-12 16:48:57 +0200

    Add asset import before testing

--- init-and-run-tests.sh
+++ init-and-run-tests.sh
@@ -2,22 +2,10 @@
 set -e
 
 GODOT_VERSION=$1
+IMPORT_SCENE="$2"
+IMPORT_TIME=$3
+TEST_TIMEOUT=$4
 
-# Unit test path
-if [ -z "$2" ]
-then
-  UNIT_TEST_PATH="res://tests/unit"
-else
-  UNIT_TEST_PATH=$2
-fi
-
-# Integration test path
-if [ -z "$3" ]
-then
-  INTEGRATION_TEST_PATH=""
-else
-  INTEGRATION_TEST_PATH="-gdir=$3"
-fi
 
 # Download Godot
 wget https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/Godot_v${GODOT_VERSION}-stable_linux_headless.64.zip
@@ -27,8 +15,16 @@ unzip Godot_v${GODOT_VERSION}-stable_linux_headless.64.zip
 mv Godot_v${GODOT_VERSION}-stable_linux_headless.64 /usr/local/bin/godot
 
 #
+# Import assets
+#
+
+echo "Importing assets for $IMPORT_TIME seconds..."
+timeout $IMPORT_TIME /usr/local/bin/godot --editor "$IMPORT_SCENE" || true
+
+#
 # Launch the tests
 #
 
+echo "Running tests (timeout at $TEST_TIMEOUT seconds)..."
 rm -f test-out.xml
-yes q | /usr/local/bin/godot -s res://addons/gut/gut_cmdln.gd -d -gjunit_xml_file=test-out.xml
+yes q | timeout $TEST_TIMEOUT /usr/local/bin/godot -s res://addons/gut/gut_cmdln.gd -d -gjunit_xml_file=test-out.xml
